549fda35aad11e2ff67968a53c09b5caed08ebcf
Zak "Ubercow" Kristjanson <ubercow1337@gmail.com>
2013-04-16 14:41:11 -0700

    Added global persistance of the slider

--- scripts/volume.js
+++ scripts/volume.js
@@ -1,8 +1,8 @@
-var player = document.getElementsByClassName("inline_player desktop-view")[0]
-if (player)
+var desktop_player = document.getElementsByClassName("inline_player desktop-view")[0]
+if (desktop_player)
 {
 	var audiotag = document.getElementsByTagName("audio")[0] 
-	player.querySelector("tr:first-child td:first-child").setAttribute("rowspan", "3")
+	desktop_player.querySelector("tr:first-child td:first-child").setAttribute("rowspan", "3")
 
 	var row = document.createElement("tr")
 	var col = row.appendChild(document.createElement("td"))
@@ -21,7 +21,7 @@ if (player)
 
 
 	// Get some stuff from the player progress bar to add style to the volume bar
-	var playprogbar = player.querySelector(".progbar_empty")
+	var playprogbar = desktop_player.querySelector(".progbar_empty")
 	var playprogbar_style = (playprogbar.currentStyle || window.getComputedStyle(playprogbar, null))
 
 	var range = document.createElement("input")
@@ -37,15 +37,25 @@ if (player)
 	range.max = 1
 	range.step = 0.01
 	range.min = 0
-	range.value = audiotag.volume
+	range.value = audiotag.value
+
+	chrome.storage.local.get("volume", function(items) {
+		audiotag.volume = items["volume"]
+		range.value = items["volume"]
+	});
+
 	range.addEventListener("change", function(evt) {
-		audiotag.volume = evt.target.value 
+		audiotag.volume = evt.target.value
+	});
+
+	range.addEventListener("mouseup", function(evt) {
+		chrome.storage.local.set({"volume":evt.target.value})
 	});
 
 	volcontainer.appendChild(input)
 	volcontainer.appendChild(range)
 	col.appendChild(volcontainer)
-	player.querySelector("tbody").appendChild(row)
+	desktop_player.querySelector("tbody").appendChild(row)
 }
 
 
